a438606c9b2d3bdbe533d903df99c8151c1c7c61
leilazarov <stoneiiilei@gmail.com>
2020-10-16 14:29:26 +1100

    Initial commit

---
+++ pet-board/demo-ui/api/api/pets.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
+import csv
+
+from flask import request, g
+
+from . import Resource
+from .. import schemas
+
+
+class Pets(Resource):
+    DB = 'pets.csv'
+
+    def get(self):
+        with open(self.DB, 'r') as csvFile:
+            csvReader = csv.DictReader(csvFile)
+            response = [line for line in csvReader]
+        return response, 200, None
+
+    def post(self):
+        row = list(g.json.values())  # or [*g.json.values()]
+        # row = list(*g.json.values())  # or [*g.json.values()]
+        with open(self.DB, 'a') as csvFile:
+            csvWriter = csv.writer(csvFile)
+            csvWriter.writerow(row)
+        return row, 201, None
---
+++ pet-board/demo-ui/api/routes.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+###
+### DO NOT CHANGE THIS FILE
+### 
+### The code is auto generated, your change will be overwritten by 
+### code generating.
+###
+from __future__ import absolute_import
+
+from .api.pets import Pets
+
+
+routes = [
+    dict(resource=Pets, urls=['/pets'], endpoint='pets'),
+]
